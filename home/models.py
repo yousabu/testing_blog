2ad1f7c0f3cc72ee0f53c7873f072aeabe1f623c
Youssef <shbanmhmdywsf8@gmail.com>
2023-02-05 17:18:42 +0200

    All operations

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from statistics import mode
 from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import datetime, date
 # Create your models here.
 
 
@@ -13,6 +13,7 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=255, default="default code")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
