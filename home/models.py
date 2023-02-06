304fca00c19060ba2229269205b3de9538026de7
Youssef <shbanmhmdywsf8@gmail.com>
2023-02-06 11:04:06 +0200

    category

--- home/models.py
+++ home/models.py
@@ -7,6 +7,16 @@ from django.contrib.auth.models import User
 from datetime import datetime, date
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title 
+
+    def get_absolute_url(self):
+        # return reverse('article-details', args=(str(self.id)))
+        return reverse('home')
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -14,6 +24,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='coding') 
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
